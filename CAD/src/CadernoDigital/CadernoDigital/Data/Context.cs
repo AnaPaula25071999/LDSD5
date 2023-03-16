a91e41d99a9b963b99368010e5914cca8cca4b90
Everton Santos <everton-r.santos@hotmail.com>
2023-03-16 20:22:22 -0300

    Projeto CAD

---
+++ CAD/src/CadernoDigital/CadernoDigital/Data/Context.cs
@@ -0,0 +1,20 @@
+﻿using CadernoDigital.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadernoDigital.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Login> Login { get; set; }
+
+    }
+}
---
+++ CAD/src/CadernoDigital/CadernoDigital/Models/Login.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadernoDigital.Models
+{
+    public class Login
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Senha { get; set; }
+        public int NivelAcesso { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public DateTime DataAtualizacao { get; set; }
+    }
+}
